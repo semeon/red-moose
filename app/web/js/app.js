fb8200d74a0669d61d8befebbe2b00a04a97dfeb
semeon <semeon.peradze@gmail.com>
2016-02-02 16:00:37 -0800

    Initial commit of the app

---
+++ app/web/js/app.js
@@ -0,0 +1,15 @@
+import {data} from "data/data.js";
+// import {Page} from "js/page/pageController.js";
+// import {Drawing} from "js/drawing/drawingController.js";
+// import {model} from "js/drawing/model/dataModel.js";
+
+// var drawing = new Drawing(model);
+// var page = new Page(drawing);
+// var defaultData = textData.text4;
+
+// page.render(defaultData);
+
+console.dir(data)
+
+
+
